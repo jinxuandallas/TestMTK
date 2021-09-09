7390d72f768e052ece32ab574b6bc2716f289753
Dallas <dallas2k@163.com>
2021-09-09 22:06:27 +0800

    基本完成了地图的功能

--- ScenarioScene/Map.gd
+++ ScenarioScene/Map.gd
@@ -1,14 +1,24 @@
 extends Node2D
 
-
+#const GRID_A=0.40
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 onready var _CurrentGrid=$CurrentGrid
-
+onready var _Grids=$Grids
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in 3:
+		for j in 2:
+			for m in 20:
+				for n in 20:
+					var grid=Sprite.new()
+					grid.texture=preload("res://Image/Misc/PositionSelector.png")
+					grid.scale=Vector2(0.5,0.5)
+					grid.position=Vector2(i*1000+m*50,j*1000+n*50)
+					grid.centered=false
+#					grid.self_modulate.a=GRID_A
+					_Grids.add_child(grid)
 
 func _physics_process(delta):
 	var current_grid=get_global_mouse_position()/50
@@ -25,3 +35,6 @@ func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _on_map_grid_pressed():
+	_Grids.visible=!_Grids.visible
+	
