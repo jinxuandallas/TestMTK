1f11f900a2b29dafcd3f860200c31367582319d6
Dallas <dallas2k@163.com>
2021-09-12 23:04:27 +0800

    做了一些修改，增加了一些json

--- Test2.gd
+++ Test2.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export var dataResource:Resource=null
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,13 +9,7 @@ export var dataResource:Resource=null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("xxx")
-#	print(dataResource.MM)
 	print(dataResource.b)
-#	print(dataResource.c)
-	
-	dataResource.b=66
-	get_tree().change_scene("res://Test/Test2.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
