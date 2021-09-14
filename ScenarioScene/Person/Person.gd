ac0c7bc5d4942df94657a13731bc114a1bbcc7a3
Dallas <dallas2k@163.com>
2021-09-14 19:40:50 +0800

    读取数据

--- ScenarioScene/Person/Person.gd
+++ ScenarioScene/Person/Person.gd
@@ -1,15 +1,52 @@
 extends Node
-
+class_name Person
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var id:int
+var surname: String 
+var given_name: String 
+var courtesy_name: String 
+
+var command: int
+var strength: int 
+var intelligence: int 
+var politics: int 
+var glamour: int
+
+var picture_index:int
+var year_available:int
+var year_born:int
+var year_dead:int
+var sex:bool
+var ideal:int
+var p_character:int
+var available_location:int
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func load_data(json:Dictionary):
+	id=json["Id"]
+	surname=json["SurName"]
+	given_name=json["GivenName"]
+	courtesy_name=json["CourtesyName"]
+	command=int(json["BaseCommand"])
+	strength=int(json["BaseStrength"])
+	intelligence=int(json["BaseIntelligence"])
+	politics=int(json["BasePolitics"])
+	glamour=int(json["BaseGlamour"])
+	picture_index=int(json["PictureIndex"])
+	year_available=int(json["YearAvailable"])
+	year_born=int(json["YearBorn"])
+	year_dead=int(json["YearDead"])
+	sex=json["Sex"]
+	ideal=int(json["Ideal"])
+	p_character=int(json["PCharacter"])
+	available_location=int(json["AvailableLocation"])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- ScenarioScene/Scenario.gd
+++ ScenarioScene/Scenario.gd
@@ -4,7 +4,7 @@ export var global_data:Resource=null
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var persons:=Dictionary()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,4 +18,13 @@ func _load_data(path):
 	var file=File.new()
 	file.open(path+"/Persons.json",File.READ)
 	var json=parse_json(file.get_as_text())
+	for item in json:
+		var instance=Person.new()
+		_load_item(instance,item,persons)
 	file.close()
+	
+#	print(persons)
+
+func _load_item(instance,item,add_to_list):
+	instance.load_data(item)
+	add_to_list[instance.id]=instance
